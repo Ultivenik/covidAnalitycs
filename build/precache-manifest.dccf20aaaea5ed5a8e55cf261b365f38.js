fac1d2bb7a85092211e7bb0342053dce87645afd
Venik <kbalibrea@gmail.com>
2022-03-01 00:47:24 +0100

    deploy

---
+++ build/precache-manifest.dccf20aaaea5ed5a8e55cf261b365f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8712eb7b46940eeffacee92d31ccf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67fd44a404a6f548cf1f",
+    "url": "/static/css/main.2db9f5ed.chunk.css"
+  },
+  {
+    "revision": "6372e121e4106d49999e",
+    "url": "/static/js/2.3eac441a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3eac441a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fd44a404a6f548cf1f",
+    "url": "/static/js/main.95e42033.chunk.js"
+  },
+  {
+    "revision": "3455081b66915ed2a5a7",
+    "url": "/static/js/runtime-main.9ac87323.js"
+  }
+]);
